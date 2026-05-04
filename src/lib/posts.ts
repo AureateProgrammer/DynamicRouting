db3df4826ebaee5fb1957ca3c66b50184e5a4c27
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-05-03 20:15:25 -0400

    Creating Blog content

---
+++ src/lib/posts.ts
@@ -0,0 +1,19 @@
+export const posts = [
+  {
+    id: "1",
+    title: "Invincible",
+    content: "Mark is my favorite character.",
+  },
+    {
+    id: "2",
+    title: "Thragg",
+    content: "This guy is a formidable opponent.",
+
+    },
+    {
+    id: "3",
+    title: " Omni Man",    
+
+    content: "He is a complex character with a dark side.",
+  },
+]
